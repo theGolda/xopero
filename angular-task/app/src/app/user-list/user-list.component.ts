8365c2d78d3e25001497d7044e606fd452eecf13
Michal Golda <michal.golda@globallogic.com>
2025-10-05 11:20:26 +0200

    add searching

--- angular-task/app/src/app/user-list/user-list.component.ts
+++ angular-task/app/src/app/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import {
   MatCell, MatCellDef,
   MatColumnDef,
@@ -10,6 +11,7 @@ import {
   MatRow, MatRowDef,
   MatTable,
   MatTableDataSource,
+  MatTableModule,
 } from '@angular/material/table'
 import { RouterModule } from '@angular/router'
 import { Store } from '@ngrx/store'
@@ -18,6 +20,9 @@ import { selectUsers } from '@store/store.selectors'
 import { UserModel } from '@models/user.model'
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-list',
@@ -25,6 +30,7 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
   styleUrls: ['./user-list.component.scss'],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     MatTable,
     MatColumnDef,
     MatHeaderCell,
@@ -37,31 +43,69 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
     MatRowDef,
     RouterModule,
     MatPaginator,
-    MatSortModule
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTableModule
   ],
 })
 export class UserListComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<UserModel>();
   users$: Observable<UserModel[]> = this.store.select(selectUsers);
+  
+  // Reactive form for filtering
+  searchForm: FormGroup;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     public store: Store,
-  ) { }
+    private fb: FormBuilder
+  ) {
+    this.searchForm = this.fb.group({
+      searchText: ['']
+    });
+  }
 
   ngOnInit(): void {
     this.store.dispatch(loadUsers());
     this.users$.pipe(
       tap(users => {
         this.dataSource.data = users;
+        this.setupFiltering();
       })
     ).subscribe();
+
+    // Subscribe to form changes for real-time filtering
+    this.searchForm.valueChanges.subscribe(() => {
+      this.applyFilter();
+    });
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+  setupFiltering(): void {
+    this.dataSource.filterPredicate = (data: UserModel, filter: string) => {
+      const searchText = this.searchForm.get('searchText')?.value?.toLowerCase() || '';
+      
+      if (!searchText) return true;
+      
+      const nameMatch = data.name?.toLowerCase().includes(searchText) || false;
+      const roleMatch = data.role?.toLowerCase().includes(searchText) || false;
+      const emailMatch = data.email?.toLowerCase().includes(searchText) || false;
+      const projectsMatch = data.protectedProjects?.toString().includes(searchText) || false;
+      
+      return nameMatch || roleMatch || emailMatch || projectsMatch;
+    };
+  }
+
+  applyFilter(): void {
+    const searchText = this.searchForm.get('searchText')?.value || '';
+    this.dataSource.filter = searchText.trim().toLowerCase();
   }
 }
